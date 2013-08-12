ad3ea6a363a7f1ba4dca98603de729f383120285
ajlopez <webmaster@ajlopez.com>
2013-08-12 17:50:36 -0300

    DynamicContext first test

---
+++ Src/SharpContext.Core.Tests/DynamicContextTests.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpContext.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DynamicContextTests
+    {
+        [TestMethod]
+        public void GetUnknownValue()
+        {
+            DynamicContext context = new DynamicContext();
+
+            Assert.IsNull(context.GetValue("Country"));
+        }
+    }
+}
