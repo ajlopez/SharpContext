1984a6bca0e121a0c3a8ef2e14297626acede9d8
ajlopez <webmaster@ajlopez.com>
2013-08-11 21:18:53 -0300

    Space retrieve first test

---
+++ Src/SharpContext.Core.Tests/RetrieveTests.cs
@@ -0,0 +1,23 @@
+﻿namespace SharpContext.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RetrieveTests
+    {
+        [TestMethod]
+        public void RetrieveEmptyContext()
+        {
+            Space<string> space = new Space<string>();
+
+            var result = space.Retrieve("Country", "UK");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Count());
+        }
+    }
+}
---
+++ Src/SharpContext.Core/Space.cs
@@ -0,0 +1,15 @@
+﻿namespace SharpContext.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Space<T>
+    {
+        public IEnumerable<T> Retrieve(params object[] args)
+        {
+            return new List<T>();
+        }
+    }
+}
